cbc2d3872735c13cb8a0c764c972b76dde9958c3
Reyno Wu <rwu@ambiq.com>
2026-02-05 11:03:52 +0800

    apollo510_AP510dL_EVB examples added HP mode

--- lvgl_ambiq_demo/lvgl_ttf/src/lvgl_ttf.c
+++ lvgl_ambiq_demo/lvgl_ttf/src/lvgl_ttf.c
@@ -72,6 +72,11 @@ main(void)
     }
 #endif
 
+    if ( am_hal_pwrctrl_mcu_mode_select(AM_HAL_PWRCTRL_MCU_MODE_HIGH_PERFORMANCE2) != AM_HAL_STATUS_SUCCESS )
+    {
+        am_util_stdio_printf("CPU enter HP mode failed!\n");
+    }
+
     //
     // Init mspi.
     //
